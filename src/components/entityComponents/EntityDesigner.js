5974a82a2ee2159f58a8bcabf60fd196ef029463
David Nickerson <dnickerson@medhok.com>
2018-11-13 13:41:14 -0500

    add fields to entity

--- src/components/entityComponents/EntityDesigner.js
+++ src/components/entityComponents/EntityDesigner.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { apiUrl } from '../../resources/apiUrl';
+import { Button, Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
 class EntityDesigner extends Component { 
   constructor(props) {
@@ -7,20 +8,36 @@ class EntityDesigner extends Component {
 
     this.state = {
       data: [],
+      inputs: ['input-0']
     };
   }
 
   render() {
-    return (<div className="App">
+    return (<div className="App" class="form-group">
       <h5><b>The Entity Designer</b></h5> 
       Available entity models:
       <ul>
-                {
-                    this.state.data.map((item, key) => {
-                       return <li key={key}>{item.name} </li>
-                    })
-                }
-            </ul>
+        {
+          this.state.data.map((item, key) => {
+            return <li key={key}>{item.name} </li>
+          })
+        }
+      </ul>
+      <hr/>
+      <center>
+      <Form class="form-inline">
+        <FormGroup controlId="formBasicText">
+          <ControlLabel>New entity: Add fields</ControlLabel>
+          {this.state.inputs.map((item, key) => {
+            return <FormControl class="form-control" type="text" name={item} key={key}/>;
+            })}
+               <Button onClick={ () => this.appendInput() }>
+                   Add Field
+               </Button>
+        </FormGroup>
+      </Form>
+      </center>
+          
       </div>);
   }
 
@@ -29,6 +46,11 @@ class EntityDesigner extends Component {
     .then(res => res.json())
     .then(data => this.setState({data}));
   }
+
+  appendInput() {
+    var newInput = `input-${this.state.inputs.length}`;
+    this.setState({ inputs: this.state.inputs.concat([newInput]) });
+}
 }
 
 export default EntityDesigner
